cd2c503f5e8207774230c9470cb22bbb5d7791ca
Calvin Tuong <calvin.tuong@gmail.com>
2013-03-13 16:09:42 +0000

    removed files

--- run_mn.sh
+++
@@ -1,2 +0,0 @@
-#sudo mn --custom ripl/ripl/mn.py --topo ft,4 --controller=remote --mac
-sudo mn --custom ripl/ripl/mn.py --topo jf,16,20,4 --controller=remote --mac
--- run_pox.sh
+++
@@ -1,2 +0,0 @@
-#pox/pox.py riplpox.riplpox --topo=ft,4 --routing=ksp --mode=reactive
-pox/pox.py riplpox.riplpox --topo=jf,16,20,4 --routing=ksp --mode=reactive
